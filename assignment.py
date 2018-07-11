3843fda68e84a929981c0c1205f4121fb7855306
Caleb Canada <calebcanada13@gmail.com>
2018-07-11 15:36:20 -0500

    Checkin of exercises

---
+++ assignment.py
@@ -0,0 +1,27 @@
+import os
+
+os.system("clear")
+
+bandMate = "ringo"
+#1.
+print("The name is this long", len(bandMate))
+#2.
+print(bandMate.upper())
+print(bandMate.lower())
+#3.
+print(bandMate[2])
+#4.
+print(bandMate[1:3])
+#5.
+print(bandMate[3:])
+#6.
+print(bandMate.title())
+print(bandMate.swapcase())
+#7.
+bM = "Terrell"
+print("My name is", bM)
+#8.
+print("The name is this long", len(bM))
+#9.
+print(bM.swapcase())
+#10.
